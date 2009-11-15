9b7a9f95a0bc79ccc572aa79b2a193e97b5d21ff
arielyang <arielyang@aa93e794-63ca-11de-b259-67f869b4f23a>
2009-11-15 04:59:08 +0000

    Reformat codes.

--- trunk/DbSharper/DbSharper.Library/Data/DatabaseFactory.cs
+++ trunk/DbSharper/DbSharper.Library/Data/DatabaseFactory.cs
@@ -1,15 +1,19 @@
-﻿////using System;
-////using System.Collections.Generic;
-////using System.Text;
-////namespace DbSharper.Library.Data
-////{
-////    public static class DatabaseFactory<TDatabase> where TDatabase : Database, new()
-////    {
-////        public static Database Create(string connectionString)
-////        {
-////            Database db = new TDatabase();
-////            db.ConnectionString = connectionString;
-////            return db;
-////        }
-////    }
-////}
+﻿#region Header
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//namespace DbSharper.Library.Data
+//{
+//	public static class DatabaseFactory<TDatabase> where TDatabase : Database, new()
+//	{
+//		public static Database Create(string connectionString)
+//		{
+//			Database db = new TDatabase();
+//			db.ConnectionString = connectionString;
+//			return db;
+//		}
+//	}
+//}
+
+#endregion Header
--- trunk/DbSharper/DbSharper.Library/GlobalSuppressions.cs
+++ trunk/DbSharper/DbSharper.Library/GlobalSuppressions.cs
@@ -7,8 +7,8 @@
 // Error List, point to "Suppress Message(s)", and click
 // "In Project Suppression File".
 // You do not need to add suppressions to this file manually.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "DbSharper.Library.Data.EnumHelper.#GetDataSource(System.Type)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Scope = "member", Target = "DbSharper.Library.Model.ItemBase.#GetValue`1(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "DbSharper.Library.Data.EnumHelper.#GetDataSource(System.Type)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1010:CollectionsShouldImplementGenericInterface", Scope = "type", Target = "DbSharper.Library.Configuration.CacheSettingCollection")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1010:CollectionsShouldImplementGenericInterface", Scope = "type", Target = "DbSharper.Library.Providers.CacheProviderCollection")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "DbSharper.Library.Caching")]
@@ -18,8 +18,9 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Json", Scope = "member", Target = "DbSharper.Library.Data.IJson.#ToJson()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Json", Scope = "type", Target = "DbSharper.Library.Data.IJson")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Json", Scope = "type", Target = "DbSharper.Library.Data.JsonBuilder")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Memcached", Scope = "type", Target = "DbSharper.Library.Providers.MemcachedCacheProvider")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "parms", Scope = "member", Target = "DbSharper.Library.Caching.CachingService.#.ctor(System.String,System.String[])")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "param", Scope = "member", Target = "DbSharper.Library.Data.Database.#ConfigureParameter(System.Data.Common.DbParameter,System.String,System.Data.DbType,System.Int32,System.Data.ParameterDirection,System.Boolean,System.Byte,System.Byte,System.String,System.Data.DataRowVersion,System.Object)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Scope = "member", Target = "DbSharper.Library.Data.Database.#DbProviderFactory")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Scope = "namespace", Target = "DbSharper.Library.Caching")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Scope = "namespace", Target = "DbSharper.Library.Configuration")]
@@ -32,7 +33,3 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "obj", Scope = "member", Target = "DbSharper.Library.Data.JsonBuilder.#.ctor(System.Object)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily", Scope = "member", Target = "DbSharper.Library.Data.JsonBuilder.#WriteValue(System.Object)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors", Scope = "member", Target = "DbSharper.Library.Model.ItemBase.#.ctor(System.Data.IDataRecord)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "param", Scope = "member", Target = "DbSharper.Library.Data.Database.#ConfigureParameter(System.Data.Common.DbParameter,System.String,System.Data.DbType,System.Int32,System.Data.ParameterDirection,System.Boolean,System.Byte,System.Byte,System.String,System.Data.DataRowVersion,System.Object)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Scope = "member", Target = "DbSharper.Library.Data.Database.#DbProviderFactory")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Scope = "namespace", Target = "DbSharper.Library.Providers.MemcachedClient")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Memcached", Scope = "namespace", Target = "DbSharper.Library.Providers.MemcachedClient")]
