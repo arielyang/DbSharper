4223479eae17c2dbaa7d2edfe0f70c9023c4ded3
arielyang <arielyang@aa93e794-63ca-11de-b259-67f869b4f23a>
2010-01-07 16:34:01 +0000

    Delete 2 files.

--- trunk/DbSharper2/DbSharper.Schema/ExtensionAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-	/// <summary>
-	/// Indicates that a method is an extension method, or that a class or assembly contains extension methods. 
-	/// </summary>
-	[AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
-	public sealed class ExtensionAttribute : Attribute { }
-}
