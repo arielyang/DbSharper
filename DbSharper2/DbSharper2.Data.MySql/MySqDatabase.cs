efa6cead305a1b7436547ac5d7d17a2f4b26f818
arielyang <arielyang@aa93e794-63ca-11de-b259-67f869b4f23a>
2010-04-07 08:02:16 +0000

    Add 2 projects for MySQL Support.

---
+++ DbSharper2/DbSharper2.Data.MySql/MySqDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+using DbSharper2.Library.Data;
+
+namespace DbSharper2.Data.MySql
+{
+	public class MySqDatabase : Database
+	{
+		#region Constructors
+
+		public MySqDatabase()
+			: base(SqlClientFactory.Instance)
+		{
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		public override string BuildParameterName(string name)
+		{
+			if (name[0] != '#')
+			{
+				return "#" + name;
+			}
+
+			return name;
+		}
+
+		#endregion Methods
+	}
+}
